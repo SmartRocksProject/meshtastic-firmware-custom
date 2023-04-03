bcd9f4452ff8c42f23a57977190704d5e7f9869e
Patrick Sullivan <psullivang7@gmail.com>
2023-04-03 14:50:49 -0400

    Prep for merge

--- src/gps/NMEAWPL.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include "main.h"
-
-uint32_t printWPL(char *buf, const Position &pos, const char *name);
-uint32_t printGGA(char *buf, const Position &pos);
--- src/platform/rp2040/architecture.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define ARCH_RP2040
-
-#if defined(PRIVATE_HW)
-    #define HW_VENDOR HardwareModel_PRIVATE_HW
-#endif
